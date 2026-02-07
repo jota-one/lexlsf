bd2b456e1cf46147b96e19544caef0f1749e9503
jorinho <jorinho@jota.one>
2026-02-07 20:10:15 +0100

    migration to allow access to all sign to the SSR calls

---
+++ pb/pb_migrations/1769773492_updated_sign.js
@@ -0,0 +1,22 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("pbc_3094607438")
+
+  // update collection data
+  unmarshal({
+    "listRule": "@request.auth.id = '' || (\n  @request.auth.roles.slug ?= 'admin' ||\n  (@request.auth.roles.slug ?= 'student' && learning_source = 'uni/m1')\n)",
+    "viewRule": "@request.auth.id = '' || (\n  @request.auth.roles.slug ?= 'admin' ||\n  (@request.auth.roles.slug ?= 'student' && learning_source = 'uni/m1')\n)"
+  }, collection)
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_3094607438")
+
+  // update collection data
+  unmarshal({
+    "listRule": "@request.auth.id != '' && (\n  @request.auth.roles.slug ?= 'admin' ||\n  (@request.auth.roles.slug ?= 'student' && learning_source = 'uni/m1')\n)",
+    "viewRule": "@request.auth.id != '' && (\n  @request.auth.roles.slug ?= 'admin' ||\n  (@request.auth.roles.slug ?= 'student' && learning_source = 'uni/m1')\n)"
+  }, collection)
+
+  return app.save(collection)
+})
