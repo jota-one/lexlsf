e68242047d30045dd45a28d9efc365560c6e167f
jorinho <jorinho@jota.one>
2026-07-18 19:17:43 +0200

    refactor: make public lexique useSigns read-only

--- src/components/lexique/composables/useSigns.ts
+++ src/components/lexique/composables/useSigns.ts
@@ -1,6 +1,5 @@
 import { ref } from 'vue'
 import useAuth from '@admin/composables/useAuth'
-import { createSlug } from '@lib/slug'
 import type { TSign } from '../../../types'
 import {
   translateNumericLevel,
@@ -9,36 +8,6 @@ import {
   primaryLanguageOptions,
   verificationStatusOptions,
 } from '@lib/signOptions'
-
-const setFormData = (payload: TSign.TForm) => {
-  const formData = new FormData()
-  if (payload.video && payload.video instanceof File) {
-    formData.append('video', payload.video)
-  }
-  formData.append('name', payload.name)
-  formData.append('definition', payload.definition)
-  formData.append('slug', createSlug(payload.name))
-  formData.append('level', translateNumericLevel(payload.level))
-  ;(payload.Category || []).forEach(cat => {
-    formData.append('Category', cat)
-  })
-  formData.append('verification_status', payload.verification_status)
-  if (payload.ConfigurationRight?.id) {
-    formData.append('ConfigurationRight', payload.ConfigurationRight.id)
-  }
-  if (payload.ConfigurationLeft?.id) {
-    formData.append('ConfigurationLeft', payload.ConfigurationLeft.id)
-  }
-  formData.append('learning_source', payload.learning_source)
-  formData.append('learning_source_detail', payload.learning_source_detail)
-  formData.append('primary_language', payload.primary_language)
-  ;(payload.Roles || []).forEach(roleId => {
-    formData.append('Roles', roleId)
-  })
-  formData.append('placement', JSON.stringify(payload.placement))
-
-  return formData
-}
 
 export default function useSigns() {
   const { pb } = useAuth()
@@ -61,30 +30,14 @@ export default function useSigns() {
     })
   }
 
-  const addSign = async (payload: any) => {
-    const formData = setFormData(payload)
-    return pb.collection('sign').create(formData)
-  }
-
-  const updateSign = async (id: string, payload: any) => {
-    const formData = setFormData(payload)
-    return pb.collection('sign').update(id, formData)
-  }
-
-  const deleteSign = async (id: string) => {
-    return pb.collection('sign').delete(id)
-  }
-
   return {
     signs,
     loadSigns,
     loadSign,
-    addSign,
+    translateNumericLevel,
     getNumericLevel,
     learningSourceOptions,
     primaryLanguageOptions,
     verificationStatusOptions,
-    deleteSign,
-    updateSign,
   }
 }
